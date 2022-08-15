9f93b70e71b12b4f8aca138a2a861714fefc6226
Lucas Falbo <95.lucas@gmail.com>
2022-08-15 18:22:46 -0300

    Initial commit

---
+++ lib/clidex.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "clidex/version"
+
+module Clidex
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/clidex/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Clidex
+  VERSION = "0.1.0"
+end
